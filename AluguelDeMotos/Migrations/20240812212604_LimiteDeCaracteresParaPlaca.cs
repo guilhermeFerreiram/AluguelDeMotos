49f4e292367592f85567b5dcfa1f1782f6c93bfe
Guilherme Ferreira <guil.ferreiram@gmail.com>
2024-08-12 18:36:06 -0300

    Definir RegularExpression para Placa

---
+++ AluguelDeMotos/Migrations/20240812212604_LimiteDeCaracteresParaPlaca.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AluguelDeMotos.Migrations
+{
+    /// <inheritdoc />
+    public partial class LimiteDeCaracteresParaPlaca : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Placa",
+                table: "Motos",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Placa",
+                table: "Motos",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+        }
+    }
+}
---
+++ AluguelDeMotos/Migrations/20240812213301_DefinirRegularExpressionParaPlaca.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AluguelDeMotos.Migrations
+{
+    /// <inheritdoc />
+    public partial class DefinirRegularExpressionParaPlaca : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Placa",
+                table: "Motos",
+                type: "character varying(8)",
+                maxLength: 8,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Placa",
+                table: "Motos",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(8)",
+                oldMaxLength: 8);
+        }
+    }
+}
