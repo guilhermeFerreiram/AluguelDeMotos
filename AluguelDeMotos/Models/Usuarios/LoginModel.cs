270be5836738ab02a17db008d9ac524de3a3d2ae
Guilherme Ferreira <guil.ferreiram@gmail.com>
2024-08-12 14:50:51 -0300

    Criar modelo LoginModel e tela de login

---
+++ AluguelDeMotos/Models/Usuarios/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluguelDeMotos.Models.Usuarios
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email obrigatório")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Senha obrigatória")]
+        public string Senha { get; set; }
+    }
+}
--- AluguelDeMotos/Program.cs
+++ AluguelDeMotos/Program.cs
@@ -33,6 +33,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Usuario}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
