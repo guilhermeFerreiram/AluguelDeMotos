186b78701d56cfc1ac9d37d4298e8c6c499c057f
Guilherme Ferreira <guil.ferreiram@gmail.com>
2024-08-12 11:35:52 -0300

    Criar tela index de Usuarios

---
+++ AluguelDeMotos/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AluguelDeMotos.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AluguelDeMotos/Program.cs
+++ AluguelDeMotos/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Usuario}/{action=Index}/{id?}");
 
 app.Run();
