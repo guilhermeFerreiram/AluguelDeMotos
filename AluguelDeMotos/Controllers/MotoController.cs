229443dc34a783b038eb5f54b3d2e71ac7fd5080
Guilherme Ferreira <guil.ferreiram@gmail.com>
2024-08-12 16:00:46 -0300

    Criar tela de cadastro de moto

--- AluguelDeMotos/Controllers/MotoController.cs
+++ AluguelDeMotos/Controllers/MotoController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AluguelDeMotos.Filters;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AluguelDeMotos.Controllers
 {
+    [SomenteAdmin]
     public class MotoController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
         {
             return View();
         }
